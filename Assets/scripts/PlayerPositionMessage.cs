bdb598da033be848ac033f0a7cd2f6d0da466868
BatteryAcid <battery4cid@gmail.com>
2021-01-09 12:47:41 -0500

    Init commit

---
+++ Assets/scripts/PlayerPositionMessage.cs
@@ -0,0 +1,19 @@
+﻿// This could be refactred into multiple message sub-classes, but to make it simple I left it alone.
+[System.Serializable]
+public class PlayerPositionMessage : GameMessage
+{
+   public SerializableVector3 velocity;
+   public SerializableVector3 enemyVelocity;
+   public int seq;
+   public string player;
+
+   public PlayerPositionMessage(string actionIn, string opcodeIn, SerializableVector3 velocityIn,
+      SerializableVector3 enemyVelocityIn, double timestamp, int seqIn, string playerIn)
+      : base(actionIn, opcodeIn)
+   {
+      velocity = velocityIn;
+      enemyVelocity = enemyVelocityIn;
+      seq = seqIn;
+      player = playerIn;
+   }
+}
